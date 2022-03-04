286d556fcddfd8165b5706d76aa9e8b7eeb9a8b9
juro.trappl <juro.trappl@gmail.com>
2022-03-04 14:21:25 +0100

    removed unnecessary discord bot setup

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
-import { Client, WebhookClient } from "discord.js";
+import { WebhookClient } from "discord.js";
 import dotenv from "dotenv";
 import job from "./job.js";
 
 dotenv.config();
 
-const discordClient = new Client({ intents: [] });
 const webhook = new WebhookClient({ url: process.env.WEBHOOK_URL });
 
 let newestPostContent = "";
-
-discordClient.on("error", ({ message }) => {
-  console.error(`Discord client error: ${message}`);
-});
-
-discordClient.once("ready", async () => {
-  job(newestPostContent, webhook).start();
-
-  console.log(`${discordClient.user.username} is ready!`);
-});
-
-discordClient.login(process.env.TOKEN);
+job(newestPostContent, webhook).start();
